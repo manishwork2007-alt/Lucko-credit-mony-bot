23200d04eccb31004615cf62e054fe9980fa6f39
manishwork2007-alt <manish.work2007@gmail.com>
2026-03-23 23:44:32 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,13 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+
+TOKEN = "8510315469:AAE_zMEIM_hKH6ARZuOrTJXTaHGFX8fy7RU"
+
+async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+    await update.message.reply_text("Tumne bola: " + text)
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(MessageHandler(filters.TEXT, reply))
+
+app.run_polling()
